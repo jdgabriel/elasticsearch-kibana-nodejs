d0d013c183969a5ef189ef90f625f1b638f701ed
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2022-11-21 16:47:11 -0300

    feat(search): Create a search folder in es server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { client } from "./clients/elasticsearch";
 import { logger } from "./clients/logger";
 
 const app = express();
@@ -8,7 +9,40 @@ app.get("/", (req: Request, res: Response) => {
   return res.status(200).json({ hello: "world" });
 });
 
+app.post("/", async (req: Request, res: Response) => {
+  const { path, description } = req.body;
+
+  //CREATE YOUR DATA WHATEVER DATABASE HERE
+
+  const createPaths = path.split("/").map((ph) => ({
+    [ph]: ph,
+  }));
+
+  const esCreated = await client.index({
+    index: "folders",
+    document: {
+      paths: createPaths,
+    },
+  });
+
+  return res.status(200).json(esCreated);
+});
+
+app.post("/search", async (req: Request, res: Response) => {
+  const { search } = req.body;
+
+  const esFind = await client.search<{ paths: { path: string }[] }>({
+    index: "folders",
+    query: {
+      match: {
+        [`paths.${search}`]: search,
+      },
+    },
+  });
+  return res.status(200).json(esFind);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-  logger.info(`Server running at http://locahost:${PORT}`);
+  logger.info(`Server running at http://localhost:${PORT}`);
 });
